23b18946d08d6dfd29386948f1556da4829486bc
Stefan Ragnar Viglundsson <Stefan Ragnar Viglundsson>
2016-10-20 14:08:52 +0000

    Fix indent

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,55 +1,53 @@
-	package is.ru.stringcalculator;
+package is.ru.stringcalculator;
 
-	public class Calculator {
+public class Calculator {
 
-		public static int add(String text){
+	public static int add(String text){
 
-			if(text.contains("-")){
-				NegativeNumbers(text);
-			}
+		if(text.contains("-")){
+			NegativeNumbers(text);
+		}
 
-			if(text.equals(""))
-				return 0;
-			
-			if(text.contains("\n")){
-				text = text.replace("\n",",");
-			}
+		if(text.equals(""))
+			return 0;
 
-			if(text.contains(",")){
-				int sum = SumOfUnknownNumbers(text);
+		if(text.contains("\n")){
+			text = text.replace("\n",",");
+		}
 
-				return sum;
-			}
+		if(text.contains(",")){
+			int sum = SumOfUnknownNumbers(text);
+			return sum;
+		}
+		else
+			return toInt(text);
+	}
+	public static int SumOfUnknownNumbers(String text){
+		String [] numbers = text.split(",");
+		int sum = 0;
+
+		for(int i = 0; i < numbers.length; i++){
+		
+			if(toInt(numbers[i]) > 1000)
+	   			numbers[i] = numbers[i+1];
 			else
-				return toInt(text);
-	    }
-
-		public static int SumOfUnknownNumbers(String text){
-			String [] numbers = text.split(",");
-			int sum = 0;
-
-			for(int i = 0; i < numbers.length; i++){
-
-				if(toInt(numbers[i]) > 1000)
-					numbers[i] = numbers[i+1];
-				else
-					sum += toInt(numbers[i]);
+				sum += toInt(numbers[i]);
 			}
 			return sum;
 		}
 
-		public static void NegativeNumbers(String text){
-			String [] numbers = text.split(",");
-			StringBuilder negatives = new StringBuilder();
-
-			for(int i = 0; i < numbers.length; i++){
-				if(toInt(numbers[i]) < 0)
-					negatives.append(numbers[i] + ",");
-			}
-			throw new IllegalArgumentException("Negatives not allowed: " + negatives.toString());
+	public static void NegativeNumbers(String text){
+		String [] numbers = text.split(",");
+		StringBuilder negatives = new StringBuilder();
+		for(int i = 0; i < numbers.length; i++){
+			if(toInt(numbers[i]) < 0)
+				negatives.append(numbers[i] + ",");
 		}
-		
-		private static int toInt(String text){
-			return Integer.parseInt(text);
-		}
+			
+		throw new IllegalArgumentException("Negatives not allowed: " + negatives.toString());
 	}
+		
+	private static int toInt(String text){
+		return Integer.parseInt(text);
+	}
+}
